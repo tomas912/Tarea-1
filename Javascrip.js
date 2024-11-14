af66d18696bceb243c5764ccd772fa7f1f07cbb8
Tomas <135394741+tomas912@users.noreply.github.com>
2024-11-14 19:51:13 -0300

    Add files via upload

---
+++ Javascrip.js
@@ -0,0 +1,13 @@
+function oscuro(){document.body.style.backgroundImage="none";document.body.style.backgroundColor= "black"; document.getElementById("header-subcontenedor").style.backgroundColor="black";
+ document.getElementById("texto-subcontenedor").style.backgroundColor="black"; document.getElementById("nav").style.backgroundColor="white";document.getElementById("nav1").style.color="black";  
+ document.getElementById("nav2").style.color="black";  document.getElementById("nav3").style.color="black";  document.getElementById("nav4").style.color="black"; 
+ document.getElementById("aside").style.backgroundColor="grey";document.getElementById("footer").style.backgroundColor="white";document.getElementById("gmail").style.color="black";
+document.getElementById("oscuro").style.display="none";document.getElementById("claro").style.display="block";
+}
+function claro(){document.body.style.backgroundImage="url(./Imagenes/MANTEL1.png)"; document.getElementById("header-subcontenedor").style.backgroundColor="crimson";
+    document.getElementById("texto-subcontenedor").style.backgroundColor="crimson"; document.getElementById("nav").style.backgroundColor="rgb(145, 21, 21)";document.getElementById("nav1").style.color="white";  
+    document.getElementById("nav2").style.color="white";  document.getElementById("nav3").style.color="white";  document.getElementById("nav4").style.color="white"; 
+    document.getElementById("aside").style.backgroundColor=" rgb(244, 238, 127)";document.getElementById("footer").style.backgroundColor="crimson";document.getElementById("gmail").style.color="white";
+   document.getElementById("oscuro").style.display="block";document.getElementById("claro").style.display="none";
+   }
+   function datos(){alert("Nombre: Tomas Duarte,Edad:17, Gmail: almarasduatomasgmail.com, Numero:11-3920-7891")}
